43adde653663e7f0ddc25260850c1c3c505939a8
James Liu <jamesjhliu@gmail.com>
2015-02-26 17:01:34 -0800

    yo angular

---
+++ client/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trafficBuddyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the trafficBuddyApp
+ */
+angular.module('trafficBuddyApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trafficBuddyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trafficBuddyApp
+ */
+angular.module('trafficBuddyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
