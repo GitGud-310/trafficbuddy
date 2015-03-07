e89197571c76f74667d4ead89ab14ffbc8fca308
William Qi <hello@williamqi.com>
2015-03-07 11:18:51 -0800

    added signup page, redirect currently dead link

--- TrafficBuddy/config/routes.rb
+++ TrafficBuddy/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'signup' => 'users#new'
+  resources :users
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
