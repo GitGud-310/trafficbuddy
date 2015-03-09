15a9ed5e3860ce7e5178382fdca067e930ad292b
James Liu <jamesjhliu@gmail.com>
2015-03-09 12:34:14 -0700

    fix utils api key helper

--- TrafficBuddy/app/assets/javascripts/utils.js
+++ TrafficBuddy/app/assets/javascripts/utils.js
@@ -1,38 +1,110 @@
 var utils = {
-    
+
     /**
-     * @returns {object} position            - Current user position
-     * @returns {number} position.latitude   - Latitude of position
-     * @returns {number} position.longitude  - Longitude of position
+     * @param   {function} [onSuccess = _geolocate_success] - callback on success
+     * @param   {function} [onError = _geolocate_error]     - callback on error
+     *
+     * @return  undefined
      */
-    geolocate: function() {
+    geolocate: function(onSuccess, onError, onUnsupported) {
+        var success = typeof onSuccess === "function" ? onSuccess : this._geolocate_success;
+        var error = typeof onError === "function" ? onError : this._geolocate_error;
         if ('geolocation' in navigator) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                    return {latitude: position.coords.latitude, longitude: position.coords.longitude};
-                },
-                function (error) {
-                    // TODO: Add error message for error in geolocation
-                });
+            navigator.geolocation.getCurrentPosition(success, error);
         } else {
-            // TODO: Add error message for unsupported broswer
+            toast("Your browser doesn't support geolocation. Update to a modern browser or use the location finder.");
         }
     },
 
     /**
-     * @params  {string} address             - User entered address/location
+     * @name    _geolocate_success
      *
-     * @returns {object} position            - Current user position
-     * @returns {number} position.latitude   - Latitude of position
-     * @returns {number} position.longitude  - Longitude of position
+     * @param   {object} position                    - position object from geolocation
+     * @param   {number} position.coords.latitude    - latitude
+     * @param   {number} position.coords.longitude   - longitude
+     *
+     * @return  undefined
      */
-    geocode: function(address) {
+    _geolocate_success: function(position) {
+        // TODO: actually implement default behaviour
+        document.getElementById("location").innerHTML = position.coords.latitude + ", " + position.coords.longitude;
+        console.log({latitude: position.coords.latitude, longitude: position.coords.longitude});
+    },
+
+    /**
+     * @name    _geolocate_error
+     *
+     * @param   {PositionError} error           - error from geolocation
+     * @param   {string}        error.message   - error message string
+     * @param   {number}        error.code      - error code
+     *          1: PERMISSION_DENIED  2: POSITION_UNAVAILABLE  3: TIMEOUT
+     *
+     * @return  undefined
+     */
+    _geolocate_error: function(error) {
+        var message = error.message === "" ? "" : ", " + error.message;
+        toast("Whoa! There was an error getting your location. Check your permissions and try again. (Error " + error.code + message + ")");
+    },
+
+    /**
+     * @param   {string} address                            - User entered address/location
+     * @param   {function} [onSuccess = _geocode_success]   - callback on success
+     * @param   {function} [onError = _geocode_error]       - callback on error
+     *
+     * @return  undefined
+     */
+    geocode: function(address, onSuccess, onError) {
+        var success = typeof onSuccess === "function" ? onSuccess : this._geocode_success;
+        var error = typeof onError === "function" ? onError : this._geocode_error;
         var geocoder = new google.maps.Geocoder();
         geocoder.geocode({'address': address}, function (results, status) {
             if (status === google.maps.GeocoderStatus.OK) {
-                return {latitude: results[0].geometry.location.lat(), longitude: results[0].geometry.location.lng()};
+                success(results);
             } else {
-                // TODO: Add error message handling for geocoder error
+                error(status);
             }
         });
+    },
+
+    /**
+     * @name    _geocode_success
+     *
+     * @param   {GeocoderResults} results                   - Google Maps geocoding results
+     * @param   {number} results[0].geometry.location.lat() - latitude
+     * @param   {number} results[0].geometry.location.lng() - longitude
+     *
+     * @return  undefined
+     */
+    _geocode_success: function(results) {
+        // TODO: actually implement default behaviour
+        console.log({latitude: results[0].geometry.location.lat(), longitude: results[0].geometry.location.lng()});
+    },
+
+    /**
+     * @name    _geocode_error
+     *
+     * @param   {string} status     - Geocoder status, one of:
+     *                              - google.maps.GeocoderStatus.ZERO_RESULTS
+     *                              - google.maps.GeocoderStatus.INVALID_REQUEST
+     *                              - google.maps.GeocoderStatus.OVER_QUERY_LIMIT:
+     *                              - google.maps.GeocoderStatus.REQUEST_DENIED:
+     *
+     * @return  undefined
+     */
+    _geocode_error: function(status) {
+        var m = "";
+        switch (status) {
+            case google.maps.GeocoderStatus.ZERO_RESULTS:
+            case google.maps.GeocoderStatus.INVALID_REQUEST:
+                m = "No results found. Enter another address and try again.";
+                break;
+            case google.maps.GeocoderStatus.OVER_QUERY_LIMIT:
+            case google.maps.GeocoderStatus.REQUEST_DENIED:
+                m = "There was an error getting your address. Please try again later.";
+                break;
+            default:
+                m = "An error occurred.";
+        }
+        toast(m);
     }
 }
--- TrafficBuddy/app/controllers/application_controller.rb
+++ TrafficBuddy/app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
   def main
+    get_api_key
     render 'main/index'
   end
 
