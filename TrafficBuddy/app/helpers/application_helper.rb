9d5842c286eeb90c61339485b94d8345d3c2070d
James Liu <jamesjhliu@gmail.com>
2015-03-09 12:34:14 -0700

    fix utils api key helper

--- TrafficBuddy/app/helpers/application_helper.rb
+++ TrafficBuddy/app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def get_api_key
+    # sets @api_key in all views to either API key set on server or a known Google Maps client key
+    # probably a magic """""number""""" but fuck it
+    @api_key = ENV['GOOGLE_CLIENT_API_KEY'] || 'AIzaSyA7HCWU3UdcM5WoeuByGBnK4cAOG7V9gJk'
+  end
 end
