fadcb5cb5134a1a0d6b149046157627114ae55c9
James Liu <jamesjhliu@gmail.com>
2015-03-12 13:27:28 -0700

    add latlon to user schema

--- TrafficBuddy/db/migrate/20150305193355_create_users.rb
+++ TrafficBuddy/db/migrate/20150305193355_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-
+      t.decimal  :latitude,  :precision => 15, :scale => 10, :default => 0.0
+      t.decimal  :longitude, :precision => 15, :scale => 10, :default => 0.0
       t.timestamps
     end
   end
