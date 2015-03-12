f9b54a5b45eda2e05472154eebde3552ea679715
James Liu <jamesjhliu@gmail.com>
2015-03-12 00:07:33 -0700

    no longer sample app lens flare sample text mum get the camera peace out cheeky scrublords

--- TrafficBuddy/app/controllers/users_controller.rb
+++ TrafficBuddy/app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to TrafficBuddy!'
       redirect_to root_path
     else
       render 'new'
