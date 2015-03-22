a352aa8b355b8763b9efa19cd690874080b2b1b9
James Liu <jamesjhliu@gmail.com>
2015-03-21 21:19:43 -0700

    Fix bug preventing sidenav/modals from working after page navigation (caused by turbolinks not triggering on document ready js event) Add Street view imagery to traffic event sidebar cards

--- TrafficBuddy/app/assets/javascripts/main.js
+++ TrafficBuddy/app/assets/javascripts/main.js
@@ -1,21 +1,25 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // //You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function(){
-// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-$('.modal-trigger').leanModal({
-    ready: function() {
-        $('#new_location, #first_name').filter(':visible').focus();
-    }
-});
+
+var ready = function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal({
+        ready: function() {
+            $('#new_location, #first_name').filter(':visible').focus();
+        }
+    });
 
     $('.button-collapse').sideNav({
         menuWidth: 300,
         edge:   'right'
     });
 
-// Show sideNav
+    // Show sideNav
     $('.button-collapse').sideNav('show');
-// Hide sideNav
+    // Hide sideNav
     $('.button-collapse').sideNav('hide');
-});
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
