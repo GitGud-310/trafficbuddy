fc03bcec463c6ed6a4074d66b8d7a74e066d7157
Shikib <mehrishikib@gmail.com>
2015-03-31 07:32:28 -0700

    added in functionality to create new planned trips object upon user looking up a planned trip.
    previously working stuff (like drawing lines between destination points) is now broken.

--- TrafficBuddy/app/controllers/planned_trips_controller.rb
+++ TrafficBuddy/app/controllers/planned_trips_controller.rb
@@ -3,6 +3,24 @@ class PlannedTripsController < ApplicationController
     @planned_trips = PlannedTrip.all
   end
 
+  def new
+    @planned_trip = PlannedTrip.new
+  end
+
+  def create
+    @planned_trip = PlannedTrip.create
+  end
+
+  def update
+    @planned_trip = PlannedTrip.find(params[:id])
+    @planned_trip.update(trips_param)
+    redirect_to root_path
+  end
+
+  def trips_param
+    params.require(:planned_trip).permit(:start_loc, :end_loc)
+  end
+
   def destroy
     PlannedTrip.find(params[:id]).destroy
     flash[:success] = "Trip removed"
