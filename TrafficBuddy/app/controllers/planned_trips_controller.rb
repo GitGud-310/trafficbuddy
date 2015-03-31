fd4f0b80379fe7b9667112de216606f5d6ce1a8a
Takeaki Yamasaki <ty.jaw6@gmail.com>
2015-03-31 16:21:04 -0700

    made planned trips only for users who are logged in

--- TrafficBuddy/app/controllers/planned_trips_controller.rb
+++ TrafficBuddy/app/controllers/planned_trips_controller.rb
@@ -5,9 +5,11 @@ class PlannedTripsController < ApplicationController
   end
 
   def create
-    @planned_trip = PlannedTrip.create()
-    @planned_trip.update_attribute(:start_loc, params[:start_location])
-    @planned_trip.update_attribute(:end_loc, params[:end_location])
+    @planned_trip = PlannedTrip.new(params[:planned_trip])
+    @planned_trip.user_id = session[:user_id]
+    @planned_trip.start_loc = params[:start_location]
+    @planned_trip.end_loc = params[:end_location]
+    @planned_trip.save
   end
 
   def destroy
@@ -17,7 +19,8 @@ class PlannedTripsController < ApplicationController
   end
 
   def destroy_all
-    PlannedTrip.destroy_all
+    @planned_trips = PlannedTrip.where(user_id: session[:user_id])
+    @planned_trips.destroy_all
     flash[:success] = "Trips removed"
     redirect_to root_path
   end
